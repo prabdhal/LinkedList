e7f043f6c0230ed597f5d9ce732c287db66205dd
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2021-02-06 11:41:41 -0500

    Add project files.

---
+++ LinkList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace LinkedList
+{
+    static class Node 
+    {
+        public int value;
+        public Node next;
+    }
+
+    static class LinkedList
+    {
+        public Node head;
+        public Node tail;
+        public static void AddFirst(object data)
+        {
+            Node newItem = new Node();
+
+            if (head == null)
+            {
+                head = newItem;
+                tail = head;
+            }
+            else
+            {
+                newItem.next = head;
+                head = newItem;
+            }
+        }
+        public static void AddLast(object data)
+        {
+            Node newItem = new Node();
+
+            if (head == null)
+            {
+                head = newItem;
+                tail = head;
+            }
+            else
+            {
+                tail.next = newItem;
+                newItem = tail;
+            }
+        }
+        public static void ReadAll()
+        {
+            Node node = head;
+
+            Console.WriteLine(node.value);
+            while (node.next != null)
+            {
+                node = node.next;
+                Console.WriteLine(node.value);
+            }
+        }
+        public static void ReturnCount()
+        {
+            Node node = head;
+            int count = 0;
+
+            if (node == null) count = 0;
+            else count = 1;
+                
+            while (node.next != null)
+            {
+                node = node.next;
+                count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
+
